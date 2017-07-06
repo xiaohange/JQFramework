0b225050a5aa5b325dbd78bc8bcdaf96199b6837
HaRi <hjq99@163.com>
2017-07-06 10:01:25 +0800

    fix:V1.0.3 新增: NSString+JQAdd

--- JQFramework/Foundation/NSString+JQAdd.h
+++ JQFramework/Foundation/NSString+JQAdd.h
@@ -2,14 +2,177 @@
 //  NSString+JQAdd.h
 //  JQFrameworkDemo
 //
-//  Created by 韩俊强 on 2017/3/15.
+//  Created by 韩俊强 on 2017/3/17.
 //  Copyright © 2017年 韩俊强. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface NSString (JQAdd)
 
+/**
+ *  是否包含字符串
+ *
+ *  @param string <#string description#>
+ *
+ *  @return <#return value description#>
+ */
+- (BOOL)containsString:(NSString *)string;
 
+/**
+ *  去除首尾空格
+ *
+ *  @return <#return value description#>
+ */
+- (NSString *)removeWhiteSpacesFromString;
+
+/**
+ *  是否为空  nil, @"", @"  ", @"\n" will Returns NO; otherwise Returns YES.
+ *
+ *  @return <#return value description#>
+ */
+-(BOOL)isNotBlank;
+/**
+ *  获取app  Version
+ *
+ *  @return <#return value description#>
+ */
++ (NSString *)getMyApplicationVersion;
+/**
+ *  获取app  名称
+ *
+ *  @return <#return value description#>
+ */
++ (NSString *)getMyApplicationName;
+/**
+ *  验证是否为邮箱
+ *
+ *  @return <#return value description#>
+ */
+- (BOOL)isValidEmail;
+/**
+ *  验证是否为电话
+ *
+ *  @return <#return value description#>
+ */
+- (BOOL)isVAlidPhoneNumber;
+/**
+ *  验证是否为URL
+ *
+ *  @return <#return value description#>
+ */
+- (BOOL)isValidUrl;
+/**
+ *  是否为中文
+ *
+ *  @return <#return value description#>
+ */
+- (BOOL)isChinese;
+
+/**
+ *  字符串替换
+ *
+ *  @param olderChar <#olderChar description#>
+ *  @param newerChar <#newerChar description#>
+ *
+ *  @return <#return value description#>
+ */
+- (NSString *)replaceCharcter:(NSString *)olderChar withCharcter:(NSString *)newerChar;
+/**
+ *  头像URl
+ *
+ *  @return <#return value description#>
+ */
+-(NSString* )headImageUrl;
+
+/**
+ *  文字占用Size
+ *
+ *  @param font          <#font description#>
+ *  @param size          <#size description#>
+ *  @param lineBreakMode <#lineBreakMode description#>
+ *
+ *  @return <#return value description#>
+ */
+- (CGSize)sizeForFont:(UIFont *)font size:(CGSize)size mode:(NSLineBreakMode)lineBreakMode;
+/**
+ *  文字占用宽度
+ *
+ *  @param font <#font description#>
+ *
+ *  @return <#return value description#>
+ */
+- (CGFloat)widthForFont:(UIFont *)font;
+
+/**
+ *  文字占用高度
+ *
+ *  @param font  <#font description#>
+ *  @param width <#width description#>
+ *
+ *  @return <#return value description#>
+ */
+- (CGFloat)heightForFont:(UIFont *)font width:(CGFloat)width;
+
+
+
+#pragma mark ----- 时间戳
+/*
+ *  时间戳对应的NSDate
+ */
+@property (nonatomic,strong,readonly) NSDate *date;
+
+/**
+ *  时间戳转格式化的时间字符串
+ */
+-(NSString *)timestampToTimeStringWithFormatString:(NSString *)formatString;
+
+/**
+ *  获取周几
+ *
+ *  @return <#return value description#>
+ */
+- (NSString *)timeStampToWeekDay;
+
+
+#pragma mark ----- 路径
+/**
+ *  application  documents路径
+ *
+ *  @return <#return value description#>
+ */
++ (NSString *)documentsPath;
+
+/**
+ *  application  cache 路径
+ *
+ *  @return <#return value description#>
+ */
++ (NSString *)cachesPath;
+
+
+/**
+ *  application library 路径
+ *
+ *  @return <#return value description#>
+ */
++ (NSString *)libraryPath;
+
+/**
+ 将某个时间 转化成 时间戳
+
+ @param format 例如：yyyy-MM-dd HH:mm:ss
+ @return 时间戳
+ */
+- (NSInteger)standardTimeStampAndFormatter:(NSString *)format;
+
+/**
+ 将某个时间戳 转化成 时间
+
+ @param format 例如：yyyy-MM-dd HH:mm:ss
+ @return 时间
+ */
+- (NSString *)timestampToStandardTimeAndFormatter:(NSString *)format;
 
 @end
--- JQFramework/Foundation/NSString+JQAdd.m
+++ JQFramework/Foundation/NSString+JQAdd.m
@@ -2,7 +2,7 @@
 //  NSString+JQAdd.m
 //  JQFrameworkDemo
 //
-//  Created by 韩俊强 on 2017/3/15.
+//  Created by 韩俊强 on 2017/3/17.
 //  Copyright © 2017年 韩俊强. All rights reserved.
 //
 
@@ -10,4 +10,236 @@
 
 @implementation NSString (JQAdd)
 
+
+
+- (BOOL)containsString:(NSString *)string {
+    if (string == nil) return NO;
+    return [self rangeOfString:string].location != NSNotFound;
+}
+
+
+- (NSString *)removeWhiteSpacesFromString {
+    NSString *trimmedString = [self stringByTrimmingCharactersInSet:
+                               [NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    return trimmedString;
+
+}
+
+-(BOOL)isNotBlank {
+    
+    NSCharacterSet *blank = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+    for (NSInteger i = 0; i < self.length; ++i) {
+        unichar c = [self characterAtIndex:i];
+        if (![blank characterIsMember:c]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
++ (NSString *)getMyApplicationVersion {
+    NSDictionary *info = [[NSBundle mainBundle] infoDictionary];
+    NSString *version = [info objectForKey:@"CFBundleVersion"];
+    return version;
+
+}
+
++ (NSString *)getMyApplicationName {
+    NSDictionary *info = [[NSBundle mainBundle] infoDictionary];
+    NSString *name = [info objectForKey:@"CFBundleDisplayName"];
+    return name;
+
+}
+
+- (BOOL)isValidEmail {
+    NSString *regex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSPredicate *emailTestPredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    return [emailTestPredicate evaluateWithObject:self];
+}
+
+- (BOOL)isVAlidPhoneNumber {
+    NSString *regex = @"^(1)\\d{10}$";
+    
+    NSPredicate *pred = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    return [pred evaluateWithObject:self];
+
+}
+
+- (BOOL)isValidUrl {
+    NSString *regex =@"(http|https)://((\\w)*|([0-9]*)|([-|_])*)+([\\.|/]((\\w)*|([0-9]*)|([-|_])*))+";
+    NSPredicate *urlTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",regex];
+    return [urlTest evaluateWithObject:self];
+
+}
+
+- (BOOL)isChinese {
+    NSString *regex =@"^[\u4E00-\u9FA5]*$";
+    NSPredicate *urlTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",regex];
+    return [urlTest evaluateWithObject:self];
+
+}
+
+
+-(NSString* )headImageUrl{
+    if ([self containsString:@"buyapp.gcimg.net"] || [self containsString:@"marketingapp.b0.upaiyun.com"]) {
+        if ([self containsString:@"!200.200"]) {
+            return self;
+        }else
+        {
+            return [self stringByAppendingString:@"!200.200"];
+        }
+    }
+    return self;
+}
+
+
+- (NSString *)replaceCharcter:(NSString *)olderChar withCharcter:(NSString *)newerChar {
+    return  [self stringByReplacingOccurrencesOfString:olderChar withString:newerChar];
+}
+
+- (CGSize)sizeForFont:(UIFont *)font size:(CGSize)size mode:(NSLineBreakMode)lineBreakMode {
+    CGSize result;
+    if (!font) font = [UIFont systemFontOfSize:12];
+    if ([self respondsToSelector:@selector(boundingRectWithSize:options:attributes:context:)]) {
+        NSMutableDictionary *attr = [NSMutableDictionary new];
+        attr[NSFontAttributeName] = font;
+        if (lineBreakMode != NSLineBreakByWordWrapping) {
+            NSMutableParagraphStyle *paragraphStyle = [NSMutableParagraphStyle new];
+            paragraphStyle.lineBreakMode = lineBreakMode;
+            attr[NSParagraphStyleAttributeName] = paragraphStyle;
+        }
+        CGRect rect = [self boundingRectWithSize:size
+                                         options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
+                                      attributes:attr context:nil];
+        result = rect.size;
+    } else {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+        result = [self sizeWithFont:font constrainedToSize:size lineBreakMode:lineBreakMode];
+#pragma clang diagnostic pop
+    }
+    return result;
+}
+
+- (CGFloat)widthForFont:(UIFont *)font {
+    CGSize size = [self sizeForFont:font size:CGSizeMake(MAXFLOAT, MAXFLOAT) mode:NSLineBreakByWordWrapping];
+    return ceill(size.width);
+}
+
+- (CGFloat)heightForFont:(UIFont *)font width:(CGFloat)width {
+    CGSize size = [self sizeForFont:font size:CGSizeMake(width, MAXFLOAT) mode:NSLineBreakByWordWrapping];
+    return ceill(size.height);
+}
+
+
+
+/*
+ *  时间戳对应的NSDate
+ */
+-(NSDate *)date{
+    
+    NSTimeInterval timeInterval=self.floatValue;
+    
+    return [NSDate dateWithTimeIntervalSince1970:timeInterval];
+}
+
+
+/**
+ *  时间戳转格式化的时间字符串
+ */
+-(NSString *)timestampToTimeStringWithFormatString:(NSString *)formatString{
+    //时间戳转date
+    NSDate *date=[NSDate dateWithTimeIntervalSince1970:[self integerValue]];
+    
+    return [self timeStringFromDate:date formatString:formatString];
+}
+
+-(NSString *)timeStringFromDate:(NSDate *)date formatString:(NSString *)formatString{
+    //实例化时间格式化工具
+    NSDateFormatter *formatter=[[NSDateFormatter alloc] init];
+    
+    //定义格式
+    formatter.dateFormat=formatString;
+    
+    //时间转化为字符串
+    NSString *dateString = [formatter stringFromDate:date];
+    
+    return dateString;
+}
+
+
+- (NSString *)timeStampToWeekDay
+{
+    NSArray *weekday = [NSArray arrayWithObjects: [NSNull null], @"星期日", @"星期一", @"星期二", @"星期三", @"星期四", @"星期五", @"星期六", nil];
+
+    NSDate *newDate = [NSDate dateWithTimeIntervalSince1970:self.floatValue];
+    
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    
+    NSDateComponents *components = [calendar components:NSCalendarUnitWeekday fromDate:newDate];
+    
+    NSString *weekStr = [weekday objectAtIndex:components.weekday];
+    return weekStr;
+}
+
+#pragma mark - 将某个时间 转化成 时间戳 -
+
+- (NSInteger)standardTimeStampAndFormatter:(NSString *)format{
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    
+    [formatter setDateStyle:NSDateFormatterMediumStyle];
+    
+    [formatter setTimeStyle:NSDateFormatterShortStyle];
+    
+    [formatter setDateFormat:format]; //(@"YYYY-MM-dd hh:mm:ss") ----------设置你想要的格式,hh与HH的区别:分别表示12小时制,24小时制
+    
+    NSTimeZone* timeZone = [NSTimeZone timeZoneWithName:@"Asia/Beijing"];
+    
+    [formatter setTimeZone:timeZone];
+    
+    NSDate* date = [formatter dateFromString:self]; //------------将字符串按formatter转成nsdate
+    
+    // 时间转时间戳的方法:
+    NSInteger timeSp = [[NSNumber numberWithDouble:[date timeIntervalSince1970]] integerValue];
+
+    return timeSp;
+}
+
+
+#pragma mark - 将某个时间戳 转化成 时间 -
+
+- (NSString *)timestampToStandardTimeAndFormatter:(NSString *)format{
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    
+    [formatter setDateStyle:NSDateFormatterMediumStyle];
+    
+    [formatter setTimeStyle:NSDateFormatterShortStyle];
+    
+    [formatter setDateFormat:format]; // （@"YYYY-MM-dd hh:mm:ss"）----------设置你想要的格式,hh与HH的区别:分别表示12小时制,24小时制
+    
+    NSTimeZone *timeZone = [NSTimeZone timeZoneWithName:@"Asia/Beijing"];
+    
+    [formatter setTimeZone:timeZone];
+    
+    NSDate *confromTimesp = [NSDate dateWithTimeIntervalSince1970:[[NSString stringWithFormat:@"%@",self] integerValue]];
+    
+    NSString *confromTimespStr = [formatter stringFromDate:confromTimesp];
+    
+    return confromTimespStr;
+}
+
+
++ (NSString *)documentsPath {
+    return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+}
+
++ (NSString *)cachesPath {
+    return [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
+}
+
++ (NSString *)libraryPath {
+    return [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) firstObject];
+}
 @end
